0e5973bd487b0fc1ba18feb3c068f81890c0e0aa
python-arch <amra51548@gmail.com>
2023-09-22 21:07:11 +0300

    Built the mvp of the nn framework

---
+++ NNFrameWork/neuron.h
@@ -0,0 +1,26 @@
+#ifndef NEURON_H
+#define NEURON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include<time.h>
+
+typedef struct neuron_t
+{
+	float actv;
+	float *out_weights;
+	float bias;
+	float z;
+
+	float dactv;
+	float *dw;
+	float dbias;
+	float dz;
+
+} neuron;
+
+neuron create_neuron(int num_out_weights);
+
+#endif
